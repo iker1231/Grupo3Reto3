18529ef8070c372e2d2b4bc33392ef9a48554064
matteo <46781101+matteofdez@users.noreply.github.com>
2024-02-01 17:31:33 +0100

    Update Peliculas.java
    POJO PELICULAS

--- reto3/src/modelo/Peliculas.java
+++ reto3/src/modelo/Peliculas.java
@@ -2,9 +2,62 @@ package modelo;
 
 public class Peliculas {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private int idPelicula;
+	private String titulo;
+	private String generoPe;
+	private String duracion;
+	/**
+	 * @return the idPelicula
+	 */
+	public int getIdPelicula() {
+		return idPelicula;
+	}
+	/**
+	 * @param idPelicula the idPelicula to set
+	 */
+	public void setIdPelicula(int idPelicula) {
+		this.idPelicula = idPelicula;
+	}
+	/**
+	 * @return the titulo
+	 */
+	public String getTitulo() {
+		return titulo;
+	}
+	/**
+	 * @param titulo the titulo to set
+	 */
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	/**
+	 * @return the generoPe
+	 */
+	public String getGeneroPe() {
+		return generoPe;
+	}
+	/**
+	 * @param generoPe the generoPe to set
+	 */
+	public void setGeneroPe(String generoPe) {
+		this.generoPe = generoPe;
+	}
+	/**
+	 * @return the duracion
+	 */
+	public String getDuracion() {
+		return duracion;
+	}
+	/**
+	 * @param duracion the duracion to set
+	 */
+	public void setDuracion(String duracion) {
+		this.duracion = duracion;
+	}
+	@Override
+	public String toString() {
+		return "Peliculas [idPelicula=" + idPelicula + ", titulo=" + titulo + ", generoPe=" + generoPe + ", duracion="
+				+ duracion + "]";
 	}
 
 }
