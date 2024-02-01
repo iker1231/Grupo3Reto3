3942b63f78f19c109900f7b157b87a557d4d2700
matteo <46781101+matteofdez@users.noreply.github.com>
2024-02-01 17:28:19 +0100

    Update Entrada.java
    Cambio para que sea como en el modelo relacional

--- reto3/src/modelo/Entrada.java
+++ reto3/src/modelo/Entrada.java
@@ -1,136 +1,54 @@
 package modelo;
 
+import java.util.ArrayList;
 import java.util.Objects;
 
 public class Entrada {
-	private int id_entrada;
-	private String fechaEntrada;
-	private String pelicula;
-	private String horario;
-	private String salaCine;
-	private float precio;
+	private int idEntrada;
+	private String fechaEmision;
+	private ArrayList<Sesion> sesiones = null;
 
 	public Entrada() {
 	}
 
-	public Entrada(int id_entrada, String fechaEntrada, String pelicula, String horario, String salaCine,
-			float precio) {
-		this.id_entrada = id_entrada;
-		this.fechaEntrada = fechaEntrada;
-		this.pelicula = pelicula;
-		this.horario = horario;
-		this.salaCine = salaCine;
-		this.precio = precio;
+	public Entrada(int id_entrada, String fechaEntrada) {
+		this.idEntrada = idEntrada;
+		this.fechaEmision = fechaEmision;
 	}
 
 	/**
-	 * @return the id_entrada
+	 * @return the idEntrada
 	 */
-	public int getId_entrada() {
-		return id_entrada;
+	public int getIdEntrada() {
+		return idEntrada;
 	}
 
 	/**
-	 * @param id_entrada the id_entrada to set
+	 * @param idEntrada the idEntrada to set
 	 */
-	public void setId_entrada(int id_entrada) {
-		this.id_entrada = id_entrada;
+	public void setIdEntrada(int idEntrada) {
+		this.idEntrada = idEntrada;
 	}
 
 	/**
-	 * @return the fechaEntrada
+	 * @return the fechaEmision
 	 */
-	public String getFechaEntrada() {
-		return fechaEntrada;
+	public String getFechaEmision() {
+		return fechaEmision;
 	}
 
 	/**
-	 * @param fechaEntrada the fechaEntrada to set
+	 * @param fechaEmision the fechaEmision to set
 	 */
-	public void setFechaEntrada(String fechaEntrada) {
-		this.fechaEntrada = fechaEntrada;
-	}
-
-	/**
-	 * @return the pelicula
-	 */
-	public String getPelicula() {
-		return pelicula;
-	}
-
-	/**
-	 * @param pelicula the pelicula to set
-	 */
-	public void setPelicula(String pelicula) {
-		this.pelicula = pelicula;
-	}
-
-	/**
-	 * @return the horario
-	 */
-	public String getHorario() {
-		return horario;
-	}
-
-	/**
-	 * @param horario the horario to set
-	 */
-	public void setHorario(String horario) {
-		this.horario = horario;
-	}
-
-	/**
-	 * @return the salaCine
-	 */
-	public String getSalaCine() {
-		return salaCine;
-	}
-
-	/**
-	 * @param salaCine the salaCine to set
-	 */
-	public void setSalaCine(String salaCine) {
-		this.salaCine = salaCine;
-	}
-
-	/**
-	 * @return the precio
-	 */
-	public float getPrecio() {
-		return precio;
-	}
-
-	/**
-	 * @param precio the precio to set
-	 */
-	public void setPrecio(float precio) {
-		this.precio = precio;
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(fechaEntrada, horario, id_entrada, pelicula, precio, salaCine);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Entrada other = (Entrada) obj;
-		return Objects.equals(fechaEntrada, other.fechaEntrada) && Objects.equals(horario, other.horario)
-				&& id_entrada == other.id_entrada && Objects.equals(pelicula, other.pelicula)
-				&& Float.floatToIntBits(precio) == Float.floatToIntBits(other.precio)
-				&& Objects.equals(salaCine, other.salaCine);
+	public void setFechaEmision(String fechaEmision) {
+		this.fechaEmision = fechaEmision;
 	}
 
 	@Override
 	public String toString() {
-		return "Entrada [id_entrada=" + id_entrada + ", fechaEntrada=" + fechaEntrada + ", pelicula=" + pelicula
-				+ ", horario=" + horario + ", salaCine=" + salaCine + ", precio=" + precio + "]";
+		return "Entrada [idEntrada=" + idEntrada + ", fechaEmision=" + fechaEmision + "]";
 	}
+
+	
 
 }
