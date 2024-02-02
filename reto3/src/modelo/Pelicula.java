dbe48d3a8994e1bd6d108de3778b5b652be51979
matteo <46781101+matteofdez@users.noreply.github.com>
2024-02-02 08:53:43 +0100

    Update Pelicula.java
    Cambio de algunos nombres

--- reto3/src/modelo/Pelicula.java
+++ reto3/src/modelo/Pelicula.java
@@ -1,63 +1,84 @@
 package modelo;
 
-public class Peliculas {
+public class Pelicula {
 
 	private int idPelicula;
 	private String titulo;
 	private String generoPe;
 	private String duracion;
+
+	/**
+	 * @param idPelicula
+	 * @param titulo
+	 * @param generoPe
+	 * @param duracion
+	 */
+	public Pelicula(int idPelicula, String titulo, String generoPe, String duracion) {
+		this.idPelicula = idPelicula;
+		this.titulo = titulo;
+		this.generoPe = generoPe;
+		this.duracion = duracion;
+	}
+
 	/**
 	 * @return the idPelicula
 	 */
 	public int getIdPelicula() {
 		return idPelicula;
 	}
+
 	/**
 	 * @param idPelicula the idPelicula to set
 	 */
 	public void setIdPelicula(int idPelicula) {
 		this.idPelicula = idPelicula;
 	}
+
 	/**
 	 * @return the titulo
 	 */
 	public String getTitulo() {
 		return titulo;
 	}
+
 	/**
 	 * @param titulo the titulo to set
 	 */
 	public void setTitulo(String titulo) {
 		this.titulo = titulo;
 	}
+
 	/**
 	 * @return the generoPe
 	 */
 	public String getGeneroPe() {
 		return generoPe;
 	}
+
 	/**
 	 * @param generoPe the generoPe to set
 	 */
 	public void setGeneroPe(String generoPe) {
 		this.generoPe = generoPe;
 	}
+
 	/**
 	 * @return the duracion
 	 */
 	public String getDuracion() {
 		return duracion;
 	}
+
 	/**
 	 * @param duracion the duracion to set
 	 */
 	public void setDuracion(String duracion) {
 		this.duracion = duracion;
 	}
+
 	@Override
 	public String toString() {
 		return "Peliculas [idPelicula=" + idPelicula + ", titulo=" + titulo + ", generoPe=" + generoPe + ", duracion="
 				+ duracion + "]";
 	}
-
 }
